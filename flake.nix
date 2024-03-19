cb6f0e1b7e2dc9897d6317161b99b1ffa85713e2
Gavin John <>
2024-03-19 18:35:20 -0500

    Fix rendering

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 

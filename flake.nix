779f101d6d415bb0f437797ff067bc2ceb0d2652
Gavin John <>
2024-03-25 14:05:33 -0500

    Migrate to NCI

--- flake.nix
+++ flake.nix
@@ -1,16 +1,78 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/23.11";
+    };
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration/53af4303dda1fe6e575b2c5ee662ac9b23a18c9f";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ { self, nixpkgs, parts, nci, ... }: parts.lib.mkFlake { inherit inputs; } (
+    let
+      cargoToml = builtins.readFile ./Cargo.toml;
+      packageData = nixpkgs.lib.getAttr "package" (builtins.fromTOML cargoToml);
+      customPackageData = nixpkgs.lib.getAttr "metadata" packageData;
+    in {
+      systems = [ "x86_64-linux" ];
+      imports = [ nci.flakeModule ];
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: let
+        crateOutputs = config.nci.outputs."${packageData.name}";
       in {
-        packages.default = import ./default.nix { inherit pkgs; };
-        devShells.default = import ./default.nix { inherit pkgs; };
-      }
-    );
+        nci = {
+          projects."${packageData.name}" = {
+            path = ./.;
+          };
+          crates."${packageData.name}" = rec {
+            depsDrvConfig = {
+              mkDerivation = with pkgs; {
+                nativeBuildInputs = [ pkg-config ];
+                buildInputs = [
+                  gtk4
+                  glib
+                  libadwaita
+                  webkitgtk_6_0
+                  gst_all_1.gst-plugins-base
+                  gst_all_1.gst-plugins-good
+                  gst_all_1.gst-plugins-bad
+                ];
+              };
+            };
+            drvConfig = {
+              mkDerivation = {
+                nativeBuildInputs = [ pkgs.pkg-config ];
+                buildInputs = depsDrvConfig.mkDerivation.buildInputs;
+                postInstall = ''
+                  mkdir -p $out/share/applications $out/share/pixmaps
+
+                  cat > $out/share/applications/${packageData.name}.desktop <<EOF
+                  [Desktop Entry]
+                  Name=${customPackageData.human_readable_name}
+                  Exec=${packageData.name}
+                  Icon=${packageData.name}
+                  Type=Application
+                  Categories=Network;WebBrowser;
+                  EOF
+
+                  # TODO: Figure out how to not hardcode release
+                  cp ./icon.svg $out/share/pixmaps/${packageData.name}.svg
+                '';
+              };
+            };
+          };
+        };
+        devShells.default = crateOutputs.devShell;
+        packages.default = crateOutputs.packages.release;
+      };
+    }
+  );
 }

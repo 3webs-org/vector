4f1caaa22f0de84c9854d8b90af4c7b8d6b1f129
Gavin John <>
2024-04-01 16:35:18 -0500

    Whoops slight build issue

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
 
 let
   cargoToml = builtins.readFile ./Cargo.toml;
-  packageData = nixpkgs.lib.getAttr "package" (builtins.fromTOML cargoToml);
-  customPackageData = nixpkgs.lib.getAttr "metadata" packageData;
 in {
   description = packageData.description;
 
@@ -22,60 +20,65 @@ in {
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, parts, nci, ... }: parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" ];
-    imports = [ nci.flakeModule ];
-    perSystem = {
-      pkgs,
-      config,
-      ...
-    }: let
-      crateOutputs = config.nci.outputs."${packageData.name}";
+  outputs = inputs @ { self, nixpkgs, parts, nci, ... }: parts.lib.mkFlake { inherit inputs; } (
+    let
+      packageData = nixpkgs.lib.getAttr "package" (builtins.fromTOML cargoToml);
+      customPackageData = nixpkgs.lib.getAttr "metadata" packageData;
     in {
-      nci = {
-        projects."${packageData.name}" = {
-          path = ./.;
-        };
-        crates."${packageData.name}" = rec {
-          depsDrvConfig = {
-            mkDerivation = with pkgs; {
-              nativeBuildInputs = [ pkg-config ];
-              buildInputs = [
-                gtk4
-                glib
-                libadwaita
-                webkitgtk_6_0
-                gst_all_1.gst-plugins-base
-                gst_all_1.gst-plugins-good
-                gst_all_1.gst-plugins-bad
-              ];
+      systems = [ "x86_64-linux" ];
+      imports = [ nci.flakeModule ];
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: let
+        crateOutputs = config.nci.outputs."${packageData.name}";
+      in {
+        nci = {
+          projects."${packageData.name}" = {
+            path = ./.;
+          };
+          crates."${packageData.name}" = rec {
+            depsDrvConfig = {
+              mkDerivation = with pkgs; {
+                nativeBuildInputs = [ pkg-config ];
+                buildInputs = [
+                  gtk4
+                  glib
+                  libadwaita
+                  webkitgtk_6_0
+                  gst_all_1.gst-plugins-base
+                  gst_all_1.gst-plugins-good
+                  gst_all_1.gst-plugins-bad
+                ];
+              };
+            };
+            drvConfig = {
+              mkDerivation = {
+                nativeBuildInputs = [ pkgs.pkg-config ];
+                buildInputs = depsDrvConfig.mkDerivation.buildInputs;
+                postInstall = ''
+                  mkdir -p $out/share/applications $out/share/pixmaps
+
+                  cat > $out/share/applications/${packageData.name}.desktop <<EOF
+                  [Desktop Entry]
+                  Name=${customPackageData.human_readable_name}
+                  Exec=${packageData.name}
+                  Icon=${packageData.name}
+                  Type=Application
+                  Categories=Network;WebBrowser;
+                  EOF
+
+                  # TODO: Figure out how to not hardcode release
+                  cp ./icon.svg $out/share/pixmaps/${packageData.name}.svg
+                '';
+              };
             };
           };
-          drvConfig = {
-            mkDerivation = {
-              nativeBuildInputs = [ pkgs.pkg-config ];
-              buildInputs = depsDrvConfig.mkDerivation.buildInputs;
-              postInstall = ''
-                mkdir -p $out/share/applications $out/share/pixmaps
-
-                cat > $out/share/applications/${packageData.name}.desktop <<EOF
-                [Desktop Entry]
-                Name=${customPackageData.human_readable_name}
-                Exec=${packageData.name}
-                Icon=${packageData.name}
-                Type=Application
-                Categories=Network;WebBrowser;
-                EOF
-
-                # TODO: Figure out how to not hardcode release
-                cp ./icon.svg $out/share/pixmaps/${packageData.name}.svg
-              '';
-            };
-          };
         };
+        devShells.default = crateOutputs.devShell;
+        packages.default = crateOutputs.packages.release;
       };
-      devShells.default = crateOutputs.devShell;
-      packages.default = crateOutputs.packages.release;
-    };
-  };
+    }
+  );
 }
